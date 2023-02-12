8e28fc92f7a1100d779922222b27ca3a9f55eb26
Kanames <kanames72@gmail.com>
2023-02-12 12:16:11 +0200

    Adding the base on the repository

---
+++ gateway-service/src/test/java/com/medium/stefan/gatewayspringclouddemo/SpringCloudGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.stefan.gatewayspringclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
